de49b5046d4129cdd89cd440050c5ddd26147ab2
Kalle Hahl <kalle.hahl@hotmail.com>
2022-09-17 14:42:02 +0300

    pieniä muutoksia, loin myös register.html filun

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def kirjaudu():
     return render_template("etusivu.html")
+
+@app.route("/register")
+def rekisteröidy():
+    return render_template("register.html")
